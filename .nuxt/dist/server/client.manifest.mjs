20efa6b7ef2fe0cd97f126e5d73098e6cbc7cc23
Ula <sarvarirvarvarik@gmail.com>
2026-02-22 15:09:30 +0500

    Fix: add .gitignore and remove ignored files

--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,4 +0,0 @@
-
-import { viteNodeFetch } from "file:///Users/ula/Documents/remont/Corporate%20Website%20Development/node_modules/@nuxt/vite-builder/dist/vite-node.mjs"
-export default () => viteNodeFetch.getManifest()
-    
